c6095e3c81b6aa29ba9e8cd5ef85a36ca7cefc9b
allwynmas <mascarenhasa@student.douglascollege.ca>
2024-08-08 19:41:07 -0700

    Project created
    new project created with all dependencies

---
+++ StudentGPTDemo/src/main/java/com/example/studentgptdemo/StudentGptDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.studentgptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentGptDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(StudentGptDemoApplication.class, args);
+    }
+
+}
---
+++ StudentGPTDemo/src/test/java/com/example/studentgptdemo/StudentGptDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentgptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGptDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
